394343a7e458af38c683b2cd3d60928a08cf9ee7
shiyj <shiyj.cn@gmail.com>
2015-03-30 20:56:32 +0800

    Initial Commit

---
+++ RTALabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RTALabel
+//
+//  Created by engin on 15-3-30.
+//  Copyright (c) 2015年 geoinker.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
